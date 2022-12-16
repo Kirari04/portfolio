beb90443d1a46fd0074e6c8ac54a4d2c016e01f6
Kirari04 <lev@saminskij.ch>
2022-12-16 20:51:38 +0100

    title position

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    <h1 class="title">
-      <TextItems :texts="lang.$.title" />
-    </h1>
-  </div>
+    <div>
+        <section class="section_title">
+            <h1 class="title">
+                <TextItems :texts="lang.$.title" />
+            </h1>
+        </section>
+    </div>
 </template>
 
 <script lang="ts" src="@/script/pages/Index.ts"></script>
 
 <style lang="scss" scoped>
-.title{
-
+.section {
+    &_title {
+      display: flex;
+      justify-content: center;
+      margin: calc(100vh / 3) 0 0 0;
+      > .title{
+        font-size: 40pt;
+      }
+    }
 }
 </style>
